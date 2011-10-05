1cbaf9364c1e89744ea62e5689185c6fad1c7f68
Martha Bernholz <mjb@unm.edu>
2011-10-05 12:54:00 -0600

    first commit

---
+++ blog3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog3::Application.config.secret_token = 'ed03bab4a8b22b829d178dce34e6797f9c6b390fe7b588ad09000d24099f81290062bf5fb8fd26ac1924d8e158b4fd3263142ac1fc1adc0a5e57ae73c0990cfd'
